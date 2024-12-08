e7838cfad85b462d65aa4632759d9b0ec7b70054
jasonlessenich <jasonlessenich@gmail.com>
2024-12-08 23:41:13 +0100

    optimized day 6 part 1

--- day06/lib/day06.dart
+++ day06/lib/day06.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:aoc/aoc.dart';
 
 enum Tile {
   empty('.'),
   obstacle('#'),
-  visited('X'),
   northGuard('^'),
   eastGuard('>'),
   southGuard('v'),
@@ -15,15 +15,6 @@ enum Tile {
   final String symbol;
 
   const Tile(this.symbol);
-
-  static Tile getGuardByDirection(Direction direction) {
-    return switch (direction) {
-      Direction.north => Tile.northGuard,
-      Direction.east => Tile.eastGuard,
-      Direction.south => Tile.southGuard,
-      Direction.west => Tile.westGuard,
-    };
-  }
 }
 
 enum Direction {
@@ -59,9 +50,26 @@ class Point {
 
   const Point(this.x, this.y);
 
+  Point move(Direction direction) {
+    return switch (direction) {
+      Direction.north => Point(x, y - 1),
+      Direction.east => Point(x + 1, y),
+      Direction.south => Point(x, y + 1),
+      Direction.west => Point(x - 1, y),
+    };
+  }
+
   @override
   String toString() {
     return '($x, $y)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other is Point) {
+      return x == other.x && y == other.y;
+    }
+    return false;
   }
 }
 
@@ -83,22 +91,21 @@ class GuardMap {
     return GuardMap(map);
   }
 
-  Tile? getTileAtPoint(Point p) {
+  bool checkBounds(Point p) {
     if (p.y < 0 || p.y >= _map.length) {
-      return null;
+      return false;
     }
     if (p.x < 0 || p.x >= _map[p.y].length) {
+      return false;
+    }
+    return true;
+  }
+
+  Tile? getTileAtPoint(Point p) {
+    if (!checkBounds(p)) {
       return null;
     }
     return _map[p.y][p.x];
-  }
-
-  Point? setTileAtPoint(Point p, Tile tile) {
-    if (getTileAtPoint(p) == null) {
-      return null;
-    }
-    _map[p.y][p.x] = tile;
-    return p;
   }
 
   (Point, Direction) getGuardPosition() {
@@ -113,61 +120,37 @@ class GuardMap {
     }
     throw StateError('No guard found!');
   }
-
-  (Point, Direction) getNextGuardPosition([Direction? directionOverride]) {
-    final (point, direction) = getGuardPosition();
-
-    final Point nextPos = switch (directionOverride ?? direction) {
-      Direction.north => Point(point.x, point.y - 1),
-      Direction.east => Point(point.x + 1, point.y),
-      Direction.south => Point(point.x, point.y + 1),
-      Direction.west => Point(point.x - 1, point.y),
-    };
-
-    return getTileAtPoint(nextPos) == Tile.obstacle ? getNextGuardPosition(direction.rotate()) : (nextPos, directionOverride ?? direction);
-  }
-
-  Point? setNextGuardPosition() {
-    final (point, _) = getGuardPosition();
-    final (nextPoint, nextDirection) = getNextGuardPosition();
-
-    setTileAtPoint(point, Tile.visited);
-    return setTileAtPoint(nextPoint, Tile.getGuardByDirection(nextDirection));
-  }
-
-  int countTiles(Tile tile) {
-    int acc = 0;
-
-    for (List<Tile> y in _map) {
-      for (Tile x in y) {
-        if (x == tile) {
-          acc++;
-        }
-      }
-    }
-
-    return acc;
-  }
 }
 
-int calculateGuardPath(GuardMap map) {
-  Point? lastGuardPos = map.getGuardPosition().$1;
-  while (lastGuardPos != null) {
-    lastGuardPos = map.setNextGuardPosition();
-  }
+int calculateSteps(GuardMap map) {
+  int steps = 0;
+  (Point?, Direction) lastPos = map.getGuardPosition();
+  List<(Point, Direction)> visited = [(lastPos.$1!, lastPos.$2)];
+  while (map.checkBounds(lastPos.$1!)) {
+    final nextPos = lastPos.$1!.move(lastPos.$2);
 
-  for (List<Tile> y in map._map) {
-    print(y.map((t) => t.symbol).join());
-  }
+    // rotate 90 degrees when encountering an obstacle
+    if (map.getTileAtPoint(nextPos) == Tile.obstacle) {
+      final Direction newDir = lastPos.$2.rotate();
+      lastPos = (lastPos.$1!.move(newDir), newDir);
+    } else {
+      lastPos = (lastPos.$1!.move(lastPos.$2), lastPos.$2);
+    }
 
-  return map.countTiles(Tile.visited);
+    // count steps (only if we haven't visited this point before)
+    if (!visited.any((e) => e.$1 == lastPos.$1)) {
+      steps++;
+      visited.add((lastPos.$1!, lastPos.$2));
+    }
+  }
+  return steps;
 }
 
 class Day06Challenge implements AOCChallenge<int> {
   @override
   FutureOr<int> part1(String input, List<String> inputLines) {
     final GuardMap map = GuardMap.fromInput(inputLines);
-    return calculateGuardPath(map);
+    return calculateSteps(map);
   }
 
   @override
